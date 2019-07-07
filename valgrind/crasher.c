a444e45ec49414aba0d8e9652cd810a711603000
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2019-07-07 21:38:08 +0300

    Some crash testing
    The C-memory management is tricky and easy to read outside what
    is allowed

---
+++ valgrind/crasher.c
@@ -0,0 +1,18 @@
+	
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv)
+{
+    for (int i = 0; i < argc; ++i)
+        printf("%d: %s\n", i, argv[i]);
+    for (int i = 0; i >= 0 ; ++i)
+    {
+        /* Read outside argv seg faults */
+        for (int j = 0; '\0' != (char)argv[i][j] ; j++)
+            putchar((char) argv[i][j]);
+        putchar('\n');
+        sleep(3);
+    }
+    return 0;
+}
