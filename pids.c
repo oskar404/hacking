c182e4988288512fe48530f25f7cfe631a4d611d
Oskar Lönnberg <k.oskar.lonnberg@gmail.com>
2019-11-23 15:42:14 +0200

    pids: Print data segment addresses
    Print the addresses of text segment end, un initiailized data
    segment end and the initialized data segment end.

--- pids.c
+++ pids.c
@@ -2,9 +2,21 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+// Explicitly declare memory layout variables
+// text segment end
+extern char etext;
+// initialized data segment end
+extern char edata;
+// uninitialized data segment end
+extern char end;
+
+
 int main()
 {
     printf("pid: %d\n",getpid());
     printf("parent pid: %d\n",getppid());
+    printf("etext: %#010x\n", (int)&etext);
+    printf("edata: %#010x\n", (int)&edata);
+    printf("end: %#010x\n", (int)&end);
     return 0;
 }
